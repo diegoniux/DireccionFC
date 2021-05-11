f42712ac7e97c0629c085a02c56e97d6e2a91d9f
cami1103 <cammilo.grimmaldo@hotmail.com>
2021-05-11 01:44:35 -0500

    barra metas component

--- src/app/shared/barra-metas/barra-metas.component.ts
+++ src/app/shared/barra-metas/barra-metas.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { DetalleGerenciasService } from 'src/app/services/detalle-gerencias-service.service';
 import { BarraMetasInterface } from 'src/app/interfaces/barraMetas.interface'
+// For MDB Angular Free
+import { PopoverModule, WavesModule } from 'angular-bootstrap-md'
 
 @Component({
   selector: 'app-barra-metas',
@@ -9,13 +11,14 @@ import { BarraMetasInterface } from 'src/app/interfaces/barraMetas.interface'
 })
 export class BarraMetasComponent implements OnInit {
 
-
+  public modalSV: string = '<div class="row"><div class="col-12"><div class="row"><div class="col-6"><p>Saldo Virtual</p></div><div class="col-6"><p>${{ barraMetas.popUpSV.saldoVirtual }}</p></div></div></div><div class="col-12"><div class="row"><div class="col-6"><p>Saldo Cantado</p></div><div class="col-6"><p>${{ barraMetas.popUpSV.saldoCantado }}</p></div></div></div><div class="col-12"><div class="row"><div class="col-6"><p>Saldo Acumulado</p></div><div class="col-6"><p>${{ barraMetas.popUpSV.saldoAcumulado }}</p></div></div></div></div>'
 
   barraMetas: BarraMetasInterface;
 
   constructor(public detalleGerenciaService: DetalleGerenciasService) {
 
     this.getBarraMetas();
+
    }
 
   ngOnInit(): void {
@@ -31,6 +34,7 @@ export class BarraMetasComponent implements OnInit {
         throw new Error(data.resultadoEjecucion.friendlyMessage);
       }
       this.barraMetas = data;
+      
     })
     .catch(error => {
       console.error(error);
