a426a010c17a6bed491ddef5f7f468e6498a0894
cami1103 <cammilo.grimmaldo@hotmail.com>
2021-05-11 19:51:23 -0500

    barra-metas

--- src/app/shared/barra-metas/barra-metas.component.ts
+++ src/app/shared/barra-metas/barra-metas.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DetalleGerenciasService } from 'src/app/services/detalle-gerencias-service.service';
-import { BarraMetasInterface } from 'src/app/interfaces/barraMetas.interface';
-// For MDB Angular Free
-import { PopoverModule, WavesModule } from 'angular-bootstrap-md';
+import { BarraMetasInterface } from 'src/app/interfaces/barraMetas.interface'
+
 
 @Component({
   selector: 'app-barra-metas',
@@ -10,8 +9,6 @@ import { PopoverModule, WavesModule } from 'angular-bootstrap-md';
   styleUrls: ['./barra-metas.component.css']
 })
 export class BarraMetasComponent implements OnInit {
-
-  public modalSV = '<div class="row"><div class="col-12"><div class="row"><div class="col-6"><p>Saldo Virtual</p></div><div class="col-6"><p>${{ barraMetas.popUpSV.saldoVirtual }}</p></div></div></div><div class="col-12"><div class="row"><div class="col-6"><p>Saldo Cantado</p></div><div class="col-6"><p>${{ barraMetas.popUpSV.saldoCantado }}</p></div></div></div><div class="col-12"><div class="row"><div class="col-6"><p>Saldo Acumulado</p></div><div class="col-6"><p>${{ barraMetas.popUpSV.saldoAcumulado }}</p></div></div></div></div>';
 
   barraMetas: BarraMetasInterface;
 
@@ -29,10 +26,12 @@ export class BarraMetasComponent implements OnInit {
     this.detalleGerenciaService.getBarraMetas()
     .toPromise()
     .then((data: BarraMetasInterface) => {
+      console.log(data);
       if (!data.resultadoEjecucion.ejecucionCorrecta) {
         throw new Error(data.resultadoEjecucion.friendlyMessage);
       }
       this.barraMetas = data;
+      
     })
     .catch(error => {
       console.error(error);
