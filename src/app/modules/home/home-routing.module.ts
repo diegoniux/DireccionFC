478996b078ac78934efc328127c697d2d5f8879b
Richi Garza <richi.garzar@gmail.com>
2021-05-25 11:08:43 -0500

    Update home-routing.module.ts

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from '../../pages/home/home.component';
+import { DetallelidercomercialComponent } from '../../pages/detallelidercomercial/detallelidercomercial.component';
+import { AuthGuard } from '../../guards/auth.guard';
 
-const routes: Routes = [{
-  path: '',
-  component: HomeComponent
-}];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'detalleLider',
+    component: DetallelidercomercialComponent,
+    canActivate: [AuthGuard]
+  }
+  ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
