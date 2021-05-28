b94bd61c9c7a5757844fc88d853ee6598628f698
Diego Nieto <dnieto@gmail.com>
2021-05-28 09:40:41 -0500

    Update reporteGerencias.interface.ts
    corregir nombre de campo sucursal

--- src/app/interfaces/reporteGerencias.interface.ts
+++ src/app/interfaces/reporteGerencias.interface.ts
@@ -4,32 +4,31 @@ export interface ReporteGerenciasInterface {
     resultadoEjecucion: ResultadoEjecucionInterface;
     reporteGerencias: ReporteGerencia[];
   }
-  
-  interface ReporteGerencia {
-    nominaGerente: string;
-    usuarioIdGerente: string;
-    susursalCve: string;
-    foto: string;
-    nombre: string;
-    apellido: string;
-    nombreSuc: string;
-    proyeccion: string;
-    proyeccionColor: string;
-    saldoAcomulado: string;
-    saldoVirtual: string;
-    saldoCantado: string;
-    saldoPromedio: string;
-    productividadEsp: string;
-    productividadEsps: ProductividadEsp[];
-    mejorSaldo: string;
-    periodo: string;
-    imgFlecha: boolean;
-    desempeno: string;
-    diagnostico: string;
-  }
-  
-  interface ProductividadEsp {
-    rangos: string;
-    especialistas: number;
-    color: string;
-  }
+
+interface ReporteGerencia {
+  nominaGerente: string;
+  usuarioIdGerente: string;
+  sucursalCve: string;
+  foto: string;
+  nombre: string;
+  apellido: string;
+  nombreSuc: string;
+  proyeccion: string;
+  proyeccionColor: string;
+  saldoAcomulado: string;
+  saldoVirtual: string;
+  saldoCantado: string;
+  saldoPromedio: string;
+  productividadEsp: string;
+  productividadEsps: ProductividadEsp[];
+  mejorSaldo: string;
+  periodo: string;
+  imgFlecha: boolean;
+  desempeno: string;
+  diagnostico: string;
+}
+interface ProductividadEsp {
+  rangos: string;
+  especialistas: number;
+  color: string;
+}
