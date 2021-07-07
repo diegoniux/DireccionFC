8995884aa4437f87e66f39d685a0a58fe8061ab0
Richi Garza <richi.garzar@gmail.com>
2021-07-07 18:06:37 -0500

    ajuste para botones

--- src/app/shared/nav-bar/nav-bar.component.ts
+++ src/app/shared/nav-bar/nav-bar.component.ts
@@ -7,6 +7,8 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
   @Input() seccionActiva:number;
+  perfilId: number;
+
   constructor() { }
 
   ngOnInit(): void {
